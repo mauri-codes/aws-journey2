aa49dee32cf857ac13d21f42f4881a85a098c24d
EC2 Default User <ec2-user@ip-172-31-64-242.ec2.internal>
2020-12-09 00:53:10 +0000

    added db-stack

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { UsersStack } from '../stacks/users-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { UsersStack } from '../stacks/users-stack'
+import { DBStack } from '../stacks/db-stack'
+
+const aws_env = { 
+   account: process.env.CDK_DEFAULT_ACCOUNT, 
+   region: process.env.CDK_DEFAULT_REGION 
+}
 
 const app = new cdk.App();
-new UsersStack(app, 'UsersStack',{ 
-   env: { 
-      account: process.env.CDK_DEFAULT_ACCOUNT, 
-      region: process.env.CDK_DEFAULT_REGION 
-   }
+new UsersStack(app, 'users-stack',{ 
+   env: aws_env
 });
+
+new DBStack(app, 'db-stack', {
+   env: aws_env
+})
